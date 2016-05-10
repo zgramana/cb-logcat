74c507a993e3468d954a2e5a03802a1aee6f2b8c
Zachary Gramana <z.gramana@me.com>
2016-05-10 16:44:09 -0700

    Updated to 1.2.1. Fixed up mkbundle script.

--- cb-logviewer/create-executable.sh
+++ cb-logviewer/create-executable.sh
@@ -4,10 +4,13 @@
 export AS="as -arch i386"
 export CC="cc -framework CoreFoundation -lobjc -liconv -arch i386"
 export MONO_PATH=/Library/Frameworks/Mono.framework/Versions/Current/
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/lib/pkgconfig:$MONO_PATH/lib/pkgconfig
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/lib/pkgconfig:$MONO_PATH/lib/pkgconfig::$MONO_PATH/bin
 export LD_LIBRARY_PATH=$MONO_PATH/lib/
+export PATH=$PATH:$MONO_PATH/bin
 
 set -e
+
+xbuild /p:Configuration=Release /t:Clean
 
 xbuild /p:Configuration=Release
 
